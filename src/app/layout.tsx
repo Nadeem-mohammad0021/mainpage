c7ee36e1ecb8c131754e62a2b8032b8078802d31
Nadeem-mohammad0021 <nadeemmohammad.0021@gmail.com>
2026-01-31 20:03:52 +0530

    Add Google Ads script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,10 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8057450154530877"
+     crossorigin="anonymous"></script>
+        
       </head>
       <body className={cn('min-h-screen font-body antialiased bg-gray-50 dark:bg-gray-950 relative')}>
         {/* Animated background elements */}
